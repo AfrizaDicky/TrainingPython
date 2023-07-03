e886a31b41b8d74a04600d4a0002765024becaa6
AfrizaDicky <138385001+AfrizaDicky@users.noreply.github.com>
2023-07-03 19:16:43 +0700

    Add files via upload

---
+++ client.py
@@ -0,0 +1,38 @@
+import paho.mqtt.client as mqtt
+import random
+
+broker = 'test.mosquitto.org'
+port = 1883
+topic = "Astronot/Data100"
+client_id = f'python-mqtt-{random.randint(0, 1000)}'
+
+def connect_mqtt() -> mqtt:
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt.Client(client_id)
+    # client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker)
+    return client
+
+
+def subscribe(client: mqtt):
+    def on_message(client, userdata, msg):
+        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+
+    client.subscribe(topic)
+    client.on_message = on_message
+
+
+def run():
+    client = connect_mqtt()
+    subscribe(client)
+    client.loop_forever()
+
+
+if __name__ == '__main__':
+    run()
